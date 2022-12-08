e24f6b444e96db1843ab2b800cf49ecbea94c9e4
Lisa-Ann B <lisabruney@pitt.edu>
2022-12-08 09:18:43 -0500

    Add provenance cypress tests

--- cypress/config/constants.js
+++ cypress/config/constants.js
@@ -3,7 +3,8 @@ const MSGS = {
     search: 'Search',
     entity: 'Entity',
     create: 'Create',
-    edit: 'Edit'
+    edit: 'Edit',
+    provenance: 'Provenance-UI'
 }
 
 const URLS = {
@@ -12,7 +13,8 @@ const URLS = {
 
 const PATHS = {
     search: `${URLS.domain}search`,
-    edit: `${URLS.domain}edit`
+    edit: `${URLS.domain}edit`,
+    view: `${URLS.domain}{entity}?uuid={id}`
 }
 
 const DATA = {
@@ -25,8 +27,8 @@ const DATA = {
         sennetId: 'SNT333.VBRT.956'
     },
     dataset: {
-        uuid: 'b7f283e49584f14a2b30abcedd712ef7',
-        sennetId: 'SNT685.CDGR.672'
+        uuid: '3ee6a090038e39af0dd536771d44caf8',
+        sennetId: 'SNT546.DPNT.455'
     }
 }
 
---
+++ cypress/e2e/provenance/page.cy.js
@@ -0,0 +1,24 @@
+import {MSGS, PATHS, DATA} from "../../config/constants";
+
+describe(`${MSGS.name}.${MSGS.provenance}.Page`, () => {
+
+    beforeEach(() => {
+        cy.login()
+        const url = PATHS.view.replace('{entity}', 'dataset').replace('{id}', DATA.dataset.uuid)
+        cy.visit(`${url}`)
+    })
+
+    context('Ensure provenance loads', () => {
+        it('Displays nodes', () => {
+            cy.get('.js-provenance .node').should('have.length.gte', 1)
+        })
+        it('Expand button loads modal with provenance', () => {
+            cy.get('.provenance--portal-ui .btn--fullView').click()
+            cy.get('.modal-full').should('have.length', 1)
+            cy.get('.modal-full .modal-title').contains('Provenance')
+            cy.get('.modal-full .js-provenance .node').should('have.length.gte', 1)
+        })
+
+    })
+
+})
