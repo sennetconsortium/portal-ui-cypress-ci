c60ba536db2bf5da8d58eeecb1c026513efea61d
Lisa-Ann B <lisabruney@pitt.edu>
2023-02-01 13:16:47 -0500

    Add rui and redirect tests

---
+++ cypress/e2e/entity/rui.cy.js
@@ -0,0 +1,18 @@
+import {MSGS, PATHS, DATA, WAIT, SELECTORS} from "../../config/constants";
+
+describe(`${MSGS.name}.${MSGS.entity}.RUI`, () => {
+    beforeEach(() => {
+        cy.login()
+    })
+
+    it('Able to select RUI tool', () => {
+        cy.visit(`${PATHS.edit}/sample?uuid=${DATA.sample.block.uuid}`)
+        cy.contains('Register location')
+    })
+
+    it('Unable to select RUI tool', () => {
+        cy.visit(`${PATHS.edit}/sample?uuid=${DATA.sample.organ.uuid}`)
+        cy.contains('Register location').should('not.exist')
+    })
+
+})
---
+++ cypress/e2e/redirect.cy.js
@@ -0,0 +1,15 @@
+import {MSGS, PATHS, DATA, WAIT, SELECTORS} from "../config/constants";
+
+describe(`${MSGS.name}.Redirect`, () => {
+
+    it('User is sent back to desired page after login', () => {
+        cy.visit(`${PATHS.edit}/sample?uuid=${DATA.sample.block.uuid}`)
+        cy.contains('Access denied')
+        cy.contains('here').click()
+        cy.wait(WAIT.time)
+        cy.loginProcess()
+        cy.wait(WAIT.time * 2)
+        cy.contains(DATA.sample.block.sennetId)
+        cy.contains('Register location')
+    })
+})
