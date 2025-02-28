a7645f2b0eed2d8841762d59b619d2148776f972
Lisa-Ann B <lisabruney@pitt.edu>
2025-02-28 11:44:42 -0500

    Update logout

--- cypress/e2e/auth/logout.cy.js
+++ cypress/e2e/auth/logout.cy.js
@@ -11,7 +11,7 @@ describe(`${MSGS.name}.Auth`, () => {
         cy.contains(AUTH.displayname).click()
         cy.get('.navbar-nav:not(.me-auto) .nav-item').eq(1).as('userMenu')
         cy.get('@userMenu').find('#nav-dropdown--user')
-        cy.get('@userMenu').find('[aria-labelledby="nav-dropdown--user"] a').eq(2).should('be.visible').click()
+        cy.get('@userMenu').find('[aria-labelledby="nav-dropdown--user"] a').eq(3).should('be.visible').click()
         cy.visit(PATHS.search)
         // TODO: Investigate why cookie deletion from ingest-api does not work within cypress
         cy.contains('Log in')
