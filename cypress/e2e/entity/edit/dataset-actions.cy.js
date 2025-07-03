96fc772ad9dd40bdca916b7628894ff6b1ff476a
Lisa-Ann B <lisabruney@pitt.edu>
2025-07-03 09:55:19 -0400

    Add tests

---
+++ cypress/e2e/entity/edit/dataset-actions.cy.js
@@ -0,0 +1,29 @@
+import {MSGS, PATHS, DATA} from "../../../config/constants";
+
+describe(`${MSGS.name}.${MSGS.entity}.${MSGS.edit}.Dataset`, () => {
+
+
+    beforeEach(() => {
+        cy.login()
+        cy.visit(`${PATHS.edit}/dataset?uuid=${DATA.dataset.rnaSeq.uuid}`)
+    })
+
+    context("Ensure success of actioning on a Dataset:", () => {
+
+        it('Can revert', () => {
+            cy.get('.btn').contains('Revert').click()
+            cy.get('.modal--ctaConfirm  select').select('New')
+            cy.get('.modal--ctaConfirm .js-btn--revert').contains('Revert').click()
+            cy.get('.MuiSvgIcon-colorSuccess').should('have.length', 1)
+        })
+
+        it('Can validate', () => {
+            cy.get('.btn').contains('Validate').click()
+            cy.get('.modal--ctaConfirm .js-btn--validate').contains('Validate').click()
+            cy.get('.MuiSvgIcon-colorSuccess').should('have.length', 1)
+        })
+
+
+    })
+
+})
--- cypress/e2e/entity/edit/dataset.cy.js
+++ cypress/e2e/entity/edit/dataset.cy.js
@@ -3,22 +3,22 @@ import {MSGS, PATHS, DATA} from "../../../config/constants";
 describe(`${MSGS.name}.${MSGS.entity}.${MSGS.edit}.Dataset`, () => {
 
     // TODO: enable if single registration ever comes in again
-    // beforeEach(() => {
-    //     cy.login()
-    //     cy.visit(`${PATHS.edit}/dataset?uuid=${DATA.dataset.rnaSeq.uuid}`)
-    // })
-    //
-    // context("Ensure success of editing Dataset:", () => {
-    //     it('Displays Hipaa', () => {
-    //         cy.checkHipaa()
-    //     })
-    //     it("Values populated", () => {
-    //         cy.get('.sui-layout-main-header').contains('SenNet ID: SN')
-    //         cy.inputValueExists(['#lab_dataset_id', '#dataset_type'])
-    //     })
-    //     it("Displays modal", () => {
-    //         cy.enterToDataset('Updated')
-    //     })
-    // })
+    beforeEach(() => {
+        cy.login()
+        cy.visit(`${PATHS.edit}/dataset?uuid=${DATA.dataset.rnaSeq.uuid}`)
+    })
+
+    context("Ensure success of editing Dataset:", () => {
+        it('Displays Hipaa', () => {
+            cy.checkHipaa()
+        })
+        it("Values populated", () => {
+            cy.get('.sui-layout-main-header').contains('SenNet ID: SN')
+            cy.inputValueExists(['#lab_dataset_id', '#dataset_type'])
+        })
+        it("Displays modal", () => {
+            cy.enterToDataset('Updated')
+        })
+    })
 
 })
