b7e0aacc5eec535e31bc8b051fd51b1024ebb355
Lisa-Ann B <lisabruney@pitt.edu>
2025-10-22 15:28:42 -0400

    Add dataset actions tests

--- cypress/e2e/entity/edit/dataset-actions.cy.js
+++ cypress/e2e/entity/edit/dataset-actions.cy.js
@@ -1,4 +1,4 @@
-import {MSGS, PATHS, DATA} from "../../../config/constants";
+import {MSGS, PATHS, DATA, WAIT} from "../../../config/constants";
 
 describe(`${MSGS.name}.${MSGS.entity}.${MSGS.edit}.Dataset`, () => {
 
@@ -8,24 +8,42 @@ describe(`${MSGS.name}.${MSGS.entity}.${MSGS.edit}.Dataset`, () => {
         cy.visit(`${PATHS.edit}/dataset?uuid=${DATA.dataset.rnaSeq.uuid}`)
     })
 
+    const revert = () => {
+        cy.get('.btn').contains('Revert').click()
+        cy.get('.modal--ctaConfirm  select').select('New')
+        cy.get('.modal--ctaConfirm .js-btn--revert').contains('Revert').click()
+    }
+
     context("Ensure success of actioning on a Dataset:", () => {
 
         it('Can revert', () => {
-            cy.get('.btn').contains('Revert').click()
-            cy.get('.modal--ctaConfirm  select').select('New')
-            cy.get('.modal--ctaConfirm .js-btn--revert').contains('Revert').click()
-            cy.get('.MuiSvgIcon-colorSuccess').should('have.length', 1)
-        })
-
-        it('Can validate', () => {
-            cy.get('.btn').contains('Validate').click()
-            cy.get('.modal--ctaConfirm .js-btn--validate').contains('Validate').click()
+            revert()
+            cy.wait(WAIT.time * 3)
             cy.get('.MuiSvgIcon-colorSuccess').should('have.length', 1)
         })
 
         it('Can process', () => {
             cy.get('.btn').contains('Process').click()
+            cy.wait(WAIT.time * 3)
             cy.get('.modal--ctaConfirm .js-btn--process').contains('Process').click()
+            cy.wait(WAIT.time * 3)
+            cy.get('.MuiSvgIcon-colorSuccess').should('have.length', 1)
+        })
+        
+        it('Can validate', () => {
+            cy.get('.btn').contains('Validate').click()
+            cy.get('.modal--ctaConfirm .js-btn--validate').contains('Validate').click()
+            cy.wait(WAIT.time * 3)
+            cy.get('.MuiSvgIcon-colorSuccess').should('have.length', 1)
+        })
+
+        it('Can submit', () => {
+            revert()
+            cy.wait(WAIT.time * 3)
+            cy.get('.btn').contains('Edit form').click()
+            cy.wait(WAIT.time)
+            cy.get('.btn').contains('Submit').click()
+            cy.get('.modal--ctaConfirm .js-btn--submit').contains('Submit').click()
             cy.get('.MuiSvgIcon-colorSuccess').should('have.length', 1)
         })
 
