31db14f1fa5a8649ff7e751b87b551d7ab650c78
Lisa-Ann B <lisabruney@pitt.edu>
2025-12-22 11:06:33 -0500

    Add transfers test

---
+++ cypress/e2e/bulk/transfers.cy.js
@@ -0,0 +1,33 @@
+import {MSGS, PATHS, DATA, SELECTORS, WAIT} from "../../config/constants";
+
+describe(`${MSGS.name}.Transfers`, () => {
+    beforeEach(() => {
+        cy.login()
+    })
+
+    const canGoToTransfers = () => {
+      cy.get('.rdt_TableCell [type="checkbox"]').eq(0).click()
+      cy.get('.c-searchActions button').click()
+      cy.contains('Transfer Files').click()
+      cy.wait(WAIT.time * 2)
+      cy.get('.rdt_TableRow').should('have.length', 1)
+    }
+
+    context("Ensure user can go to transfers page", () => {
+
+        it('Can tranfer files from Search Entities', () => {
+            cy.visit(PATHS.search)
+            cy.facets('Dataset', null)
+            canGoToTransfers()
+        })
+
+        it('Can tranfer files from Search Files', () => {
+            cy.visit(PATHS.searchFiles)
+            canGoToTransfers()
+        })
+
+      
+
+    })
+
+})
